51a080a8903a73cb7d8ff12ba16ba4d913ba9902
Egor Prokopov <egorprokopov216@gmail.com>
2024-08-24 15:05:00 +0300

    add tower mixers

---
+++ OmniFusion/merge_of_encoders/tower_mixer.py
@@ -0,0 +1,32 @@
+import torch
+import torch.nn as nn
+
+
+class WeightedSumMixer(nn.Module):
+    def __init__(self, in_dim, out_dim):
+        super().__init__()
+
+        self.weight_1 = nn.Parameter(torch.randn(1,))
+        self.weight_2 = nn.Parameter(torch.randn(1,))
+
+    def forward(self, x1: torch.Tensor, x2: torch.Tensor) -> torch.Tensor:
+        z = self.weight_1 * x1 + self.weight_2 + x2
+        return z
+
+
+class MLPMixer(nn.Module):
+    def __init__(self, in_dim, out_dim):
+        super().__init__()
+
+        self.in_dim = in_dim
+        self.out_dim = out_dim
+
+        self.mlp = nn.Sequential(
+            nn.Linear(self.in_dim, self.out_dim), nn.GELU(), nn.Linear(out_dim, out_dim)
+        )
+
+    def forward(self, x1: torch.Tensor, x2: torch.Tensor) -> torch.Tensor:
+        x = torch.cat((x1, x2), dim=1)
+        # assert x.shape[1] == x1.shape[1] + x2.shape[1]
+        out = self.mlp(x)
+        return out
