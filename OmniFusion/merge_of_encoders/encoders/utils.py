f9379e5e15c1db429e1413b773e554091a04e03c
Egor Prokopov <egorprokopov216@gmail.com>
2024-08-26 16:21:49 +0300

    sizes emb_dim fix

--- OmniFusion/merge_of_encoders/encoders/utils.py
+++ OmniFusion/merge_of_encoders/encoders/utils.py
@@ -4,16 +4,16 @@ import torch
 def initialize_special_embs(emb_dim=896, device="cpu", dtype=torch.float16):
     special_embs = {}
     special_embs['USER'] = torch.normal(
-        torch.zeros(emb_dim), torch.ones(emb_dim) / emb_dim ** 0.5
+        torch.zeros((emb_dim, )), torch.ones((emb_dim, )) / emb_dim ** 0.5
     ).to(device=device, dtype=dtype)
     special_embs['BOT'] = torch.normal(
-        torch.zeros(emb_dim), torch.ones(emb_dim) / emb_dim ** 0.5
+        torch.zeros((emb_dim, )), torch.ones((emb_dim, )) / emb_dim ** 0.5
     ).to(device=device, dtype=dtype)
     special_embs['SOI'] = torch.normal(
-        torch.zeros(emb_dim), torch.ones(emb_dim) / emb_dim ** 0.5
+        torch.zeros((emb_dim, )), torch.ones((emb_dim, )) / emb_dim ** 0.5
     ).to(device=device, dtype=dtype)
     special_embs['EOI'] = torch.normal(
-        torch.zeros(emb_dim), torch.ones(emb_dim) / emb_dim ** 0.5
+        torch.zeros((emb_dim, )), torch.ones((emb_dim, )) / emb_dim ** 0.5
     ).to(device=device, dtype=dtype)
 
     special_embs['SOI'].requires_grad_()
