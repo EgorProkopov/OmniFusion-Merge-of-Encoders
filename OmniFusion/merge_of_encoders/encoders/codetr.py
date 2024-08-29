fe90cb3e162e2d61ae406f4672549f9a6183fde6
Max <makstislenko@gmail.com>
2024-08-29 13:17:36 +0400

    od train fix

--- OmniFusion/merge_of_encoders/encoders/codetr.py
+++ OmniFusion/merge_of_encoders/encoders/codetr.py
@@ -62,11 +62,13 @@ class CoDETRVisionTower(nn.Module):
         if type(images) is list:
             image_features = []
             for image in images:
+                self.vision_tower = self.vision_tower.to(device=self.device, dtype=self.dtype)
                 image_forward_out = self.vision_tower(image.to(device=self.device, dtype=self.dtype).unsqueeze(0),
                                                       output_hidden_states=True)
                 image_feature = self.feature_select(image_forward_out).to(image.dtype)
                 image_features.append(image_feature)
         else:
+            self.vision_tower=self.vision_tower.to(device=self.device, dtype=self.dtype)
             image_forward_outs = self.vision_tower(images.to(device=self.device, dtype=self.dtype),
                                                    output_hidden_states=True)
             image_features = self.feature_select(image_forward_outs).to(images.dtype)
